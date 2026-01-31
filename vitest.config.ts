f07c16e065299e6827e0d71ded30c4aef1da5194
ks6088ts <ks6088ts@gmail.com>
2026-01-31 09:14:42 +0900

    make update

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     includeSource: ["src/**/*.ts", "test/**/*.ts"],
+    exclude: ["**/node_modules/**", "**/dist/**"],
   },
 });
